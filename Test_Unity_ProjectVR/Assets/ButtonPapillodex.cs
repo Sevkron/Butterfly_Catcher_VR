6ca76bf0614940c33b1a01a3714e7f5fd643ddb2
sevkron <sevkron@gmail.com>
2019-11-15 18:30:17 +0100

    Papillodex Start + Grab Finish

---
+++ Test_Unity_ProjectVR/Assets/ButtonPapillodex.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+namespace Valve.VR.InteractionSystem.Sample
+{
+    public class ButtonPapillodex : MonoBehaviour
+    {
+        public Canvas m_PapillodexCanvas;
+        private bool CanvasOpen = false;
+        public void OnButtonDown(Hand fromHand)
+        {
+            fromHand.TriggerHapticPulse(1000);
+            CanvasOpen = !CanvasOpen;
+            Debug.Log("Pressed");
+
+            if(CanvasOpen == true)
+            {
+                m_PapillodexCanvas.enabled = true;
+            }
+            else
+            {
+                m_PapillodexCanvas.enabled = false;
+            }
+        }
+
+        public void OnButtonUp(Hand fromHand)
+        {
+
+        }
+    }
+}
