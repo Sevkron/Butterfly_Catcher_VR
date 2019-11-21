27f0a04d58a7bdb4cb8159d0c96c3d102f0ad37a
sevkron <sevkron@gmail.com>
2019-11-21 15:52:23 +0100

    J'espère que ça va marcher

---
+++ Test_Unity_ProjectVR/Assets/Scripts/PapillodexUIButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using UnityEngine.UI;
+
+namespace Valve.VR.InteractionSystem.Sample
+{
+public class PapillodexUIButton : MonoBehaviour
+{
+        public GameObject m_IndexPanel;
+        public GameObject m_MainMenuPanel;
+        public GameObject m_PreviousPanel;
+        public void OnButtonDown(Hand fromHand)
+        {
+            fromHand.TriggerHapticPulse(1000);
+            fromHand.otherHand.useControllerHoverComponent = false;
+            Debug.Log("Pressed");
+            m_IndexPanel.SetActive(true);
+            this.gameObject.SetActive(false);
+        }
+
+        public void OnButtonUp(Hand fromHand)
+        {
+
+        }
+    }
+}
