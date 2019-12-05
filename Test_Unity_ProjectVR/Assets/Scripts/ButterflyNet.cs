649f7d1372d87897ed319f4e4dbe1469841f016e
sevkron <sevkron@gmail.com>
2019-12-05 23:02:48 +0100

    Fixed update have butterfly locked to net, deactivate navmesh

---
+++ Test_Unity_ProjectVR/Assets/Scripts/ButterflyNet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+using UnityEngine.AI;
+
+public class ButterflyNet : MonoBehaviour
+{
+    public BoxCollider firstCollider;
+    public SphereCollider secondCollider;
+    public GameObject exitedButterfly;
+    public BehaviorTree butterflyBehaviorTree;
+    public Transform netTransform;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == exitedButterfly)
+        {
+            butterflyBehaviorTree = exitedButterfly.GetComponent<BehaviorTree>();
+            exitedButterfly.GetComponent<NavMeshAgent>().enabled = false;
+            butterflyBehaviorTree.SetVariableValue("Transform", this.transform);
+            butterflyBehaviorTree.SendEvent<object>("CapturedInNet", netTransform.position);
+            Debug.Log("Butterfly received");
+        }
+    }
+}
