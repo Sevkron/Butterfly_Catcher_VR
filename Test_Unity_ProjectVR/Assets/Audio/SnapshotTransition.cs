ec04505df3ac4886a6493c85222dc1ffd6ec4cab
XaoPaiq <lenibeg9@gmail.com>
2020-01-06 16:21:00 +0100

    AudioMixer Sur scene Léni et snapshots fonctionnel

---
+++ Test_Unity_ProjectVR/Assets/Audio/SnapshotTransition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SnapshotTransition : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public AudioMixerSnapshot[] snapshots;
+    public float[] timeToReach;
+    public float timeToReachTampon;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            audioMixer.TransitionToSnapshots(snapshots, timeToReach, 3.0f);
+            timeToReachTampon = timeToReach[0];
+            timeToReach[0] = timeToReach[1];
+            timeToReach[1] = timeToReachTampon;
+        }
+    }
+
+}
