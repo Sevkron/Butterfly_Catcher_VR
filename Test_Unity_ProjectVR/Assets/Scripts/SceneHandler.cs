5b19ac78527338969ab2cfd85bda413340b9da81
sevkron <sevkron@gmail.com>
2020-01-07 15:41:53 +0100

    Grab step 1

--- Test_Unity_ProjectVR/Assets/Scripts/SceneHandler.cs
+++ Test_Unity_ProjectVR/Assets/Scripts/SceneHandler.cs
@@ -7,47 +7,47 @@ using Valve.VR.Extras;
 
 public class SceneHandler : MonoBehaviour
 {
-    public SteamVR_LaserPointer laserPointer;
+    public SteamVR_LaserPointer laserPointerRight;
 
     void Awake()
     {
-        laserPointer.PointerIn += PointerInside;
-        laserPointer.PointerOut += PointerOutside;
-        laserPointer.PointerClick += PointerClick;
+        laserPointerRight.PointerIn += PointerInside;
+        laserPointerRight.PointerOut += PointerOutside;
+        laserPointerRight.PointerClick += PointerClick;
     }
 
     public void PointerClick(object sender, PointerEventArgs e)
     {
-        if (e.target.name == "Cliff_F")
+        if (e.target.name == "Cube")
         {
-            Debug.Log("Cube was clicked");
+            Debug.Log("Cube was clicked right");
         } else if (e.target.name == "Button")
         {
-            Debug.Log("Button was clicked");
+            Debug.Log("Right Button was clicked");
         }
     }
 
     public void PointerInside(object sender, PointerEventArgs e)
     {
-        if (e.target.name == "Cliff_F")
+        if (e.target.name == "Cube")
         {
-            Debug.Log("Cube was entered");
+            Debug.Log("Cube was entered right");
         }
         else if (e.target.name == "Button")
         {
-            Debug.Log("Button was entered");
+            Debug.Log("Right Button was entered");
         }
     }
 
     public void PointerOutside(object sender, PointerEventArgs e)
     {
-        if (e.target.name == "Cliff_F")
+        if (e.target.name == "Cube")
         {
-            Debug.Log("Cube was exited");
+            Debug.Log("Cube was exited right");
         }
         else if (e.target.name == "Button")
         {
-            Debug.Log("Button was exited");
+            Debug.Log("Button was exited right");
         }
     }
 }
