ff365231aa8e88c1d9137cd423e89e71fb0ce000
XaoPaiq <lenibeg9@gmail.com>
2020-01-07 14:55:54 +0100

    AmbianceJapon + Snapshot amelioré

--- Test_Unity_ProjectVR/Assets/Audio/SnapshotTransition.cs
+++ Test_Unity_ProjectVR/Assets/Audio/SnapshotTransition.cs
@@ -9,6 +9,9 @@ public class SnapshotTransition : MonoBehaviour
     public AudioMixerSnapshot[] snapshots;
     public float[] timeToReach;
     public float timeToReachTampon;
+    public float transitionTime;
+    public AudioSource plaineAmbiance;
+    public AudioSource japonAmbiance;
 
     void Start()
     {
@@ -19,10 +22,28 @@ public class SnapshotTransition : MonoBehaviour
     {
         if (Input.GetKeyDown("e"))
         {
-            audioMixer.TransitionToSnapshots(snapshots, timeToReach, 3.0f);
+            audioMixer.TransitionToSnapshots(snapshots, timeToReach, transitionTime);
             timeToReachTampon = timeToReach[0];
             timeToReach[0] = timeToReach[1];
             timeToReach[1] = timeToReachTampon;
+            StartCoroutine(WaitToCutSong());
+            
+        }
+
+
+        IEnumerator WaitToCutSong()
+        {
+            yield return new WaitForSeconds(transitionTime*2);
+            if (timeToReach[0] == 1)
+            {
+                plaineAmbiance.Play();
+                japonAmbiance.Stop();
+            }
+            else
+            {
+                japonAmbiance.Play();
+                plaineAmbiance.Stop();
+            }
         }
     }
 
