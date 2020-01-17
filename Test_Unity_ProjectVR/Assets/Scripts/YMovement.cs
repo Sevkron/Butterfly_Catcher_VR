7e006fc2c14225c6e62dae19ef422334d2ad1bf5
NathanaelAngevin <nathanaelangevin@gmail.com>
2020-01-17 15:52:15 +0100

    Papillon qui monte et descned youpi

--- Test_Unity_ProjectVR/Assets/Scripts/YMovement.cs
+++ Test_Unity_ProjectVR/Assets/Scripts/YMovement.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class YMovement : MonoBehaviour
 {
-    public float smoothTime = .1f;
-    public float translation;
+    public float ysmoothTime = .3f;
+    public float xsmoothTime = 0.5f;
+    public float ymini =  - 1 ;
+    public float ymax = 2.5f;
+    public float xmini = -1;
+    public float xmax = 1.5f;
     private float translationVel;
+
     
     [HideInInspector] public new Transform transform;
 
@@ -17,10 +22,12 @@ public class YMovement : MonoBehaviour
 
     private void Update()
     {
-        float yPos = Mathf.SmoothDamp(transform.localPosition.y, Random.Range(-translation, translation), ref translationVel, smoothTime);
+        float yPos = Mathf.SmoothDamp(transform.localPosition.y, Random.Range(ymini, ymax), ref translationVel, ysmoothTime);
         //transform.localPosition = new Vector3(transform.localPosition.x, yPos, transform.localPosition.z);
 
-        float xPos = Mathf.SmoothDamp(transform.localPosition.x, Random.Range(-translation, translation), ref translationVel, smoothTime);
+        float xPos = Mathf.SmoothDamp(transform.localPosition.x, Random.Range(xmini , xmax), ref translationVel, xsmoothTime);
         transform.localPosition = new Vector3(xPos, yPos, transform.localPosition.z);
+
+        //transform.LookAt(- transform.localPosition);
     }
 }
