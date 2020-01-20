97100c4194b1f2a4a370a8f87cb9f9c66205b836
XaoPaiq <lenibeg9@gmail.com>
2020-01-20 16:48:33 +0100

    AmbianceGrotte+Clairière+AudioMixer Mal Gerer

--- Test_Unity_ProjectVR/Assets/Audio/SnapshotTransition.cs
+++ Test_Unity_ProjectVR/Assets/Audio/SnapshotTransition.cs
@@ -12,6 +12,8 @@ public class SnapshotTransition : MonoBehaviour
     public float transitionTime;
     public AudioSource plaineAmbiance;
     public AudioSource japonAmbiance;
+    public AudioSource grotteAmbiance;
+    public AudioSource clairiereAmbiance;
 
     void Start()
     {
@@ -20,29 +22,64 @@ public class SnapshotTransition : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown("e"))
+        if (Input.GetKeyDown("e")) // JaponPlaine
         {
             audioMixer.TransitionToSnapshots(snapshots, timeToReach, transitionTime);
             timeToReachTampon = timeToReach[0];
             timeToReach[0] = timeToReach[1];
             timeToReach[1] = timeToReachTampon;
+            StartCoroutine(WaitToCutSong());  
+        }
+
+        if (Input.GetKeyDown("r")) // JaponGrotte
+        {
+            audioMixer.TransitionToSnapshots(snapshots, timeToReach, transitionTime);
+            timeToReachTampon = timeToReach[1];
+            timeToReach[1] = timeToReach[2];
+            timeToReach[2] = timeToReachTampon;
             StartCoroutine(WaitToCutSong());
-            
+        }
+
+        if (Input.GetKeyDown("t")) // JaponClairière
+        {
+            audioMixer.TransitionToSnapshots(snapshots, timeToReach, transitionTime);
+            timeToReachTampon = timeToReach[1];
+            timeToReach[1] = timeToReach[3];
+            timeToReach[3] = timeToReachTampon;
+            StartCoroutine(WaitToCutSong());
         }
 
 
         IEnumerator WaitToCutSong()
         {
             yield return new WaitForSeconds(transitionTime/2);
-            if (timeToReach[0] == 1)
+            if (timeToReach[0] == 1) //Plaine
             {
                 plaineAmbiance.Play();
                 japonAmbiance.Stop();
+                grotteAmbiance.Stop();
+                clairiereAmbiance.Stop();
             }
-            else
+            if (timeToReach[1] == 1) //Japon
             {
                 japonAmbiance.Play();
                 plaineAmbiance.Stop();
+                grotteAmbiance.Stop();
+                clairiereAmbiance.Stop();
+            }
+            if (timeToReach[2] == 1) //Grotte
+            {
+                japonAmbiance.Stop();
+                plaineAmbiance.Stop();
+                grotteAmbiance.Play();
+                clairiereAmbiance.Stop();
+            }
+            if (timeToReach[3] == 1) //Clairiere
+            {
+                japonAmbiance.Stop();
+                plaineAmbiance.Stop();
+                grotteAmbiance.Stop();
+                clairiereAmbiance.Play();
             }
         }
     }
