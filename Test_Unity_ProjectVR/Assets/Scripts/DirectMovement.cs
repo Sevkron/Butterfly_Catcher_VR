c1e2971ea9a3a0b967554f477fef19b8728c7fad
sevkron <sevkron@gmail.com>
2020-02-25 17:38:15 +0100

    Player capsule alignée

--- Test_Unity_ProjectVR/Assets/Scripts/DirectMovement.cs
+++ Test_Unity_ProjectVR/Assets/Scripts/DirectMovement.cs
@@ -19,6 +19,7 @@ public class DirectMovement : MonoBehaviour
     public AudioClip directMoveSound;
 
     private CharacterController characterController;
+    public GameObject m_camera;
     public float speed = 1;
     private float forwardAxis;
     void Start()
@@ -30,6 +31,7 @@ public class DirectMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        speed = input.axis.y;
         forwardAxis = input.axis.y;
         //backwards move
         /*if(input.axis.y < 0)
@@ -39,9 +41,7 @@ public class DirectMovement : MonoBehaviour
 
         Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(0, 0, forwardAxis));
         characterController.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up) - new Vector3(0, 9.81f, 0) * Time.deltaTime);
-        //truc
-        //Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(Input.axis.x, 0, Input.axis.y));
-        //characterController.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(direction,Vector3.up));
+        characterController.center = new Vector3(Player.instance.hmdTransform.localPosition.x, characterController.center.y, Player.instance.hmdTransform.localPosition.z);
     }
 }
 
