b1d793e51af5fe2df5de6432effc88c4e5bf5b1e
sevkron <sevkron@gmail.com>
2020-02-25 17:44:58 +0100

    direct Move done

--- Test_Unity_ProjectVR/Assets/Scripts/DirectMovement.cs
+++ Test_Unity_ProjectVR/Assets/Scripts/DirectMovement.cs
@@ -30,6 +30,7 @@ public class DirectMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        speed = input.axis.y;
         forwardAxis = input.axis.y;
         //backwards move
         /*if(input.axis.y < 0)
@@ -39,6 +40,7 @@ public class DirectMovement : MonoBehaviour
 
         Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(0, 0, forwardAxis));
         characterController.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up) - new Vector3(0, 9.81f, 0) * Time.deltaTime);
+        characterController.center = new Vector3(Player.instance.hmdTransform.localPosition.x, characterController.center.y, Player.instance.hmdTransform.localPosition.z);
         //truc
         //Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(Input.axis.x, 0, Input.axis.y));
         //characterController.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(direction,Vector3.up));
