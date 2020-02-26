e56e4f29427cd0c6d53bd8ea4f8228377fee96ff
sevkron <sevkron@gmail.com>
2020-02-26 21:34:49 +0100

    Noise distance method

---
+++ Test_Unity_ProjectVR/Assets/TeleportNoiseDistance.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportNoiseDistance : MonoBehaviour
+{
+    public float radiusNoise;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Physics.SphereCastAll(transform.position, radiusNoise, transform.forward, out hit);
+        
+    }
+}
