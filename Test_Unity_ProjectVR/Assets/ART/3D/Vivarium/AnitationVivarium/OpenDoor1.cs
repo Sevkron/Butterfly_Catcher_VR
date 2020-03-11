64fe4085c2a0ac75dd2c4dab53e37a56be631f72
XaoPaiq <lenibeg9@gmail.com>
2020-03-11 15:39:18 +0100

    VivaDorz

---
+++ Test_Unity_ProjectVR/Assets/ART/3D/Vivarium/AnitationVivarium/OpenDoor1.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor1 : MonoBehaviour
+{
+
+    public GameObject vivarium;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+
+            vivarium.GetComponent<Animator>().SetBool("PlayerNearDoor1", true);
+            FindObjectOfType<AudioManager>().Play("OpenVivarium");
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+
+            vivarium.GetComponent<Animator>().SetBool("PlayerNearDoor1", false);
+            FindObjectOfType<AudioManager>().Play("CloseVivarium");
+
+        }
+    }
+}
--- Test_Unity_ProjectVR/Assets/Audio/Script/AudioManager.cs
+++ Test_Unity_ProjectVR/Assets/Audio/Script/AudioManager.cs
@@ -22,10 +22,9 @@ public class AudioManager : MonoBehaviour
 //        DontDestroyOnLoad(gameObject);
         foreach (Sound s in sounds)
         {
-            //s.source.clip = s.clip;
+            s.source.clip = s.clip;
 
             s.source.volume = s.volume;
-            s.source.pitch = s.pitch;
 
             s.source.loop = s.loop;
         }
@@ -33,11 +32,11 @@ public class AudioManager : MonoBehaviour
 
     public void Start()
     {
-        //Play("MusicPlaine");
     }
     public void Play (string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.clip = s.clip;
         if (s == null)
         {
             Debug.LogWarning("Sound: " + name + " not found!");
