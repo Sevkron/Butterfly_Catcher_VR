fe76fb14b97beac45fb6df0c8b4ae0895850c8d6
NathanaelAngevin <nathanaelangevin@gmail.com>
2020-09-03 13:03:59 +0100

    Timer + Sphere

---
+++ Test_Unity_ProjectVR/Assets/timer/Timer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    Image fillImg;
+    public float timer = 10;
+    float time;
+    public Text timeText;
+
+    // Use this for initialization
+    void Start()
+    {
+        fillImg = this.GetComponent<Image>();
+        time = timer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+            fillImg.fillAmount = time / timer;
+            timeText.text = "Time : " + time.ToString("F");
+        }
+    }
+}
