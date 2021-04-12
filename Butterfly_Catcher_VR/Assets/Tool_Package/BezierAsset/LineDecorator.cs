9ad2e925712911ca8958bcc666f81aae556516d0
Sevkron <47613907+Sevkron@users.noreply.github.com>
2021-04-12 17:26:17 +0200

    Trail renderer + cleanup scripts

---
+++ Butterfly_Catcher_VR/Assets/Tool_Package/BezierAsset/LineDecorator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.ProBuilder;
+
+public class LineDecorator : MonoBehaviour
+{
+    public bool lookForward;
+
+    void Start()
+    {
+
+    }
+}
